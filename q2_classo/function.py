918b1969c4c5763da7e2fc1cd6932449e00d1960
Leo-Simpson <52931376+Leo-Simpson@users.noreply.github.com>
2020-03-23 17:13:44 +0100

    first update

---
+++ q2_classo/function.py
@@ -0,0 +1,2 @@
+# defining the functions
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+      name="q2-classo",
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      packages=find_packages(),
+      author="Matthew Ryan Dillon",
+      author_email="matthewrdillon@gmail.com",
+      url="https://qiime2.org",
+      license="BSD-3-Clause",
+      description="Taxonomic analysis and visualization.",
+      entry_points={
+      "qiime2.plugins":
+      ["q2-taxa=q2_taxa.plugin_setup:plugin"]
+      },
+      package_data={'q2_taxa': ['assets/barplot/index.html',
+                                'assets/barplot/dist/*']},
+      zip_safe=False,
+      )
